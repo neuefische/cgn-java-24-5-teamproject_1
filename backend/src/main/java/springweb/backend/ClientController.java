99a46cdb0d6afe18df171ae5000009a2d4128d67
Tobias Weber <toby-weber@gmx.de>
2024-11-14 19:20:58 +0100

    add post and delete requests for client and product in shopping list

--- backend/src/main/java/springweb/backend/ClientController.java
+++ backend/src/main/java/springweb/backend/ClientController.java
@@ -17,17 +17,32 @@ public class ClientController {
     }
 
     @GetMapping
-    public List<Client> getAllGroceryProducts() {
+    public List<Client> getAllClients() {
         return clientService.getAllClients();
     }
 
-
-
-    /*
-    @PostMapping
-    public GroceryProduct addGroceryProduct(@RequestBody GroceryProduct groceryProduct) {
-        return clientService.addGroceryProduct(groceryProduct);
+    @GetMapping({"id"})
+    public List<GroceryProduct> getAllGroceryProductsFromClient(@PathVariable String id) {
+        return clientService.getAllGroceryProductsFromClient(id);
     }
-    * */
 
+    @PostMapping("{id}/shoppingList")
+    public Client addGroceryProductToClient(@PathVariable String id, @RequestBody GroceryProduct groceryProduct) {
+        return clientService.addGroceryProductToClient(groceryProduct, id);
+    }
+
+    @PostMapping
+    public Client addClient(@RequestBody Client client) {
+        return clientService.addClient(client);
+    }
+
+    @DeleteMapping({"id"})
+    public void deleteClientById(@PathVariable String id) {
+        clientService.deleteClientById(id);
+    }
+
+    @DeleteMapping("{idClient}/shoppingList{idProduct}")
+    public void deleteProductByIdFromClientById(@PathVariable String idClient, @PathVariable String idProduct) {
+        clientService.deleteProductByIdFromClientById(idClient,idProduct);
+    }
 }
--- backend/src/main/java/springweb/backend/ClientService.java
+++ backend/src/main/java/springweb/backend/ClientService.java
@@ -3,6 +3,8 @@ package springweb.backend;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 
 @Service
 public class ClientService {
@@ -19,10 +21,65 @@ public class ClientService {
 
 
 
- /*
-    public GroceryProduct addGroceryProduct(GroceryProduct groceryProduct) {
+    public Client addGroceryProductToClient(GroceryProduct groceryProduct,String id) {
 
-        return clientRepository.save(groceryProduct);
+        if (clientRepository.existsById(id)){
+            GroceryProduct newGroceryProduct = new GroceryProduct(
+                    groceryProduct.id(),
+                    groceryProduct.category(),
+                    groceryProduct.name(),
+                    groceryProduct.price(),
+                    groceryProduct.image());
+
+            Client client = clientRepository.findById(id).get();
+            client.shoppingList().add(newGroceryProduct);
+            return clientRepository.save(client);
+        }else {
+            throw new NoSuchElementException("No Client found with Id:" + id);
+        }
     }
- * */
+
+    public Client addClient(Client client) {
+        return clientRepository.save(client);
+    }
+
+    public boolean deleteClientById(String id) {
+        if (clientRepository.existsById(id)){
+            clientRepository.deleteById(id);
+            return true;
+        }else {
+            throw new NoSuchElementException("No Client found with Id:" + id);
+        }
+    }
+
+    public List<GroceryProduct> getAllGroceryProductsFromClient(String id) {
+        if (clientRepository.existsById(id)){
+            return clientRepository.findById(id).get().shoppingList();
+        }
+        throw new NoSuchElementException("No Client found with Id:" + id);
+    }
+
+    public void deleteProductByIdFromClientById(String idClient, String idProduct) {
+        if (clientRepository.existsById(idClient)){
+            Client client = clientRepository.findById(idClient).get();
+            boolean productExists = client.shoppingList()
+                    .stream()
+                    .anyMatch(product -> product.id().equals(idProduct));
+            if (!client.shoppingList().isEmpty() && productExists){
+
+                List<GroceryProduct> filteredList = client.shoppingList()
+                        .stream()
+                        .filter(product -> !product.id().equals(idProduct))
+                        .collect(Collectors.toList());
+
+                client.shoppingList().clear();
+                client.shoppingList().addAll(filteredList);
+
+                clientRepository.save(client);
+            }
+            throw new NoSuchElementException("No Product found with Id:" + idProduct);
+
+        }
+        throw new NoSuchElementException("No Client found with Id:" + idClient);
+    }
 }
