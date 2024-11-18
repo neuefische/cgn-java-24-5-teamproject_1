17a181334722c22cf253825a9baa9db2b811eada
Tobias Weber <toby-weber@gmx.de>
2024-11-18 13:57:38 +0100

    add count and status

--- frontend/src/Product.ts
+++ frontend/src/Product.ts
@@ -1,9 +1,12 @@
 import {Category} from "./Category.ts";
+import {Status} from "./Status.ts";
 
 export type Product = {
     id: number,
     name: string,
     category: Category,
     price: number,
+    count: number,
+    status: Status,
     image: string
 }
